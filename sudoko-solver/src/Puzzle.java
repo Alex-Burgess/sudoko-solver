f971ec4d2a04a7d7d757cdc05075354e9de658ff
budge1 <burgess.alexander@gmail.com>
2014-11-02 22:42:42 +0000

    First commit

--- sudoko-solver/src/Puzzle.java
+++ sudoko-solver/src/Puzzle.java
@@ -1,8 +1,17 @@
+import java.util.ArrayList;
 
 public class Puzzle {
+	ArrayList<int[]> rows = new ArrayList<int[]>();
 	
 	public Puzzle(){
-		
+		// Create 9 arrays (rows) of size 9 (columns).
+		for (int i = 0; i < 9; i++ )
+			rows.add(new int[9]);
+	}
+	
+	public void printPuzzle(){
+		for (int i = 0; i < 9; i++ )
+			System.out.println(rows.get(i));
 	}
 
 }
