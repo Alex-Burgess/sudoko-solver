9ff43d00ac3a2f8228091b03d5435c4bb53edc12
budge1 <burgess.alexander@gmail.com>
2014-11-03 09:17:04 +0000

    Work on train, to solve unit tests up to test 8.

--- sudoko-solver/src/Puzzle.java
+++ sudoko-solver/src/Puzzle.java
@@ -10,16 +10,72 @@ public class Puzzle {
 			rows.add(new int[9]);
 	}
 	
-	// TODO finish
-	public int getCell(int columnNUm, int rowNum){
-		int cellValue = 1;
-		
-		return cellValue; 
+	public int getCell(int columnNum, int rowNum){
+		int[] row = rows.get(rowNum);	// Get the row array in question
+		return row[columnNum]; 
 	}
 	
-	// TODO finish
-	public void setCell(int columnNUm, int rowNum){
+	public void setCell(int columnNum, int rowNum, int value) throws Exception
+	{
+		// Check if the value is between 1 and 9.
+		if(value < 1 || value > 9)
+			throw new Exception("Rule violation: Value being added to cell is not in the range of 1-9.");
 		
+		int[] row = rows.get(rowNum);	// Get the row array in question
+		
+		if(row[columnNum] != 0)
+			throw new Exception("Rule violation: Cell already contains a value. Column ("+ columnNum +"), Row ("+ rowNum +").");
+		
+		if(checkRow(rowNum, value) == false)
+			throw new Exception("Rule violation: Row already contains a value. Column ("+ columnNum +"), Row ("+ rowNum +").");
+		
+		if(checkColumn(columnNum, value) == false)
+			throw new Exception("Rule violation: Column already contains a value. Column ("+ columnNum +"), Row ("+ rowNum +").");
+		
+		row[columnNum] = value;
+	}
+	
+	public boolean checkRow(int rowNum, int value)
+	{
+		// Checks that the value does not exist in the row.  False means it is already present.
+		int[] row = rows.get(rowNum);
+		
+		for(int i = 0; i < 9; i++)
+			if(row[i] == value)
+				return false;
+		
+		return true;
+	}
+	
+	public boolean checkColumn(int columnNum, int value)
+	{
+		// Checks that the value does not exist in the column.  False means it is already present.
+		for(int i = 0; i < 9; i++)
+		{
+			int[] row = rows.get(i);
+			
+			if(row[columnNum] == value)
+				return false;
+		}
+		
+		return true;
+	}
+	
+//	public int getGridNum(int columnNum, int rowNum){
+//		int gridNum;
+//		
+//		return gridNum;
+//	}
+	
+	public boolean checkGrid(int columnNum, int rowNum, int value)
+	{
+		// Checks whether the grid number entered contains the value.
+		
+		//getGridNum
+		
+		// TODO How to traverse the grid?
+		
+		return true;
 	}
 	
 	public void printPuzzle(){
